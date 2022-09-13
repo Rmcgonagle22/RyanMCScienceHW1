e7469177482f4ca895251087ed476940e7bbbd73
rmcgonagle <rmcgonagle@student.bridgew.edu>
2022-09-12 20:36:06 -0400

    comitted1

---
+++ main.py
@@ -0,0 +1,11 @@
+your_name = input("What should I call you:")
+your_major = input("What is your major?")
+message1 = 'I said "you want a single string" '
+message2 = " I can't understand what use double quotes"
+# message3 = " he said "that would be bad" last night"
+message4 = '''This string can't be stopped by anything even 
+newlines and "that is what the prof said" stuff'''
+print(message4)
+comment_to_user = f"wow {your_name} comp151 will really help you for {your_major}"
+print("wow", your_name, "is really a neat name", "nice!", your_major, "sounds very cool!")
+print(comment_to_user)
